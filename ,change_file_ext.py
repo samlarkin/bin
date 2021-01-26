37aa04884dcf2af36a19aa54b140aa952763e211
Sam Larkin <selarkin96@gmail.com>
2021-01-26 16:57:54 +0000

    extend usability of ,change_file_ext.py by adding user inputs

--- ,change_file_ext.py
+++ ,change_file_ext.py
@@ -2,8 +2,13 @@
 import os
 import re
 
-for f in os.listdir('.'):
-    if re.match(r'.+\.raw', f):
+directory = input('Enter the absolute path of the directory:\n')
+from_ext = input("Change files with extension [e.g. .txt]:\n")
+to_ext = input("To files with extension [e.g. .md]:\n")
+regex = re.compile(f'.+{from_ext}')
+
+for f in os.listdir(directory):
+    if regex.match(f):
         (name, ext) = f.split('.')
-        cmd = f"mv {f} {name + '.txt'}"
+        cmd = f"mv {f} {name + to_ext}"
         os.system(cmd)
